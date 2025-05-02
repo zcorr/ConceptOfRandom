6284aa2b54b7c3dbc433f753253145676ef7efe4
Iván Dombiak <idombiak@gmail.com>
2025-05-01 17:52:29 -0700

    Added basic animation

--- ConceptOfRandom/View/MenuOutline.cs
+++ ConceptOfRandom/View/MenuOutline.cs
@@ -4,7 +4,7 @@ using ConsoleRenderer;
 public class MenuOutline : IConsoleCanvas{
 	// Instance variable of our console renderer
 	private ConsoleCanvas _canvas = new ConsoleCanvas();
-	private double _framerate = 60;
+	private double _framerate = 30;
 	private DateTime _previousframe = DateTime.Now;
 	
 	
@@ -15,17 +15,26 @@ public class MenuOutline : IConsoleCanvas{
 	public void Tick() {
 		_canvas.Clear();
 		_canvas.CreateBorder();
-		
+
 		var currentTime = DateTime.Now;
-		if ((currentTime - _previousframe).TotalMilliseconds >= _framerate) {
-			// animations would in here every tick
-			
+		if ((currentTime - _previousframe).TotalMilliseconds >= 1000 / _framerate) {
+			var text = "Wave Animation!";
+			var width = _canvas.Width;
+			var height = 5; // Adjust wave height
+			var frequency = 0.05; // Lower frequency for smoother motion
+
+			for (var i = 0; i < text.Length; i++) {
+				var x = i + (width - text.Length) / 2; // Center horizontally
+				var y = (int)(Math.Sin((i + currentTime.Ticks / 10000000.0) * frequency) * height / 2 + height / 2);
+				_canvas.Set(x, y, text[i], ConsoleColor.White, ConsoleColor.Black);
+			}
+
 			_previousframe = currentTime;
 		}
 
 		_canvas.AutoResize = true;
-		_canvas.Set(250, 250, ConsoleColor.DarkBlue);
 		_canvas.Render();
+		System.Threading.Thread.Sleep(50);
 	}
 
 	public void CreateBorder() {
